d07ba133c82c66ad7e10f83cdb4631b6bacd33ef
nhoda <nicohodali@gmail.com>
2024-01-30 16:02:26 -0300

    add: auto-animate, add: create task

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref } from 'vue';
+import { vAutoAnimate } from '@formkit/auto-animate'
 
 const taskList = ref([{
   id: 1,
@@ -23,28 +24,51 @@ const statusEmoji = (status) => {
   if (status === 'in-progress') return '⚙️'
   return '✔️'
 }
+
+const formData = ref({
+  id: 0,
+  description: '',
+  status: 'not-done',
+  title: ''
+})
+
+const handleSubmit = () => {
+  const newTask = {
+    id: Date.now(),
+    description: formData.value.description,
+    status: formData.value.status,
+    title: formData.value.title
+  }
+
+  taskList.value = [...taskList.value, newTask]
+
+  formData.value = {
+    id: 0,
+    description: '',
+    status: 'not-done',
+    title: ''
+  }
+}
 </script>
 
 <template>
   <main>
-    <form class="task-form" @submit.prevent>
+    <form class="task-form" @submit.prevent="handleSubmit">
       <label for="title">Title</label>
-      <input type="text" id="title" />
-
+      <input type="text" id="title" v-model="formData.title" required />
       <label for="description">Description</label>
-      <textarea name="descrtiption" id="description"></textarea>
-
+      <textarea name="descrtiption" id="description" v-model="formData.description" required></textarea>
       <label for="status">Status</label>
-      <select name="status" id="status">
-        <option value="done">To Do</option>
+      <select name="status" id="status" v-model="formData.status">
+        <option value="not-done">To Do</option>
         <option value="in-progress">In Progress</option>
-        <option value="not-done">Not Done</option>
+        <option value="done">Done</option>
       </select>
 
       <button>Create Task</button>
     </form>
 
-    <ul class="task-list">
+    <ul class="task-list" v-auto-animate>
       <li class="task-item" v-for="task in taskList" :key="task.id">
         <h3>{{ task.title }}</h3>
         <p>{{ task.description }}</p>
@@ -56,6 +80,5 @@ const statusEmoji = (status) => {
         </div>
       </li>
     </ul>
-
   </main>
 </template>
